164bf8bad98589e8fc6c481bb898b613959e796c
Bryan Van 't veld <61687852+bryanV030V@users.noreply.github.com>
2024-01-09 13:22:29 +0100

    Create MenuController.cs
    add menu controller

---
+++ Script/MenuController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject paintingGame; // Drag the GameObject with the PaintingGame script attached to this field in the Inspector
+    public GameObject menuPanel;
+
+    void Start()
+    {
+        // Ensure the menu is hidden initially
+        menuPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Open/Close menu with Escape key
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleMenu();
+        }
+    }
+
+    public void SaveCanvas()
+    {
+        // Call the SaveCanvasFromMenu function in PaintingGame script
+        paintingGame.GetComponent<PaintingGame>().SaveCanvasFromMenu();
+    }
+
+    public void ContinueGame()
+    {
+        // Close the menu
+        ToggleMenu();
+    }
+
+    public void QuitGame()
+    {
+        // Quit the game (only works in a built executable)
+        Application.Quit();
+    }
+
+    void ToggleMenu()
+    {
+        // Toggle the visibility of the menu
+        menuPanel.SetActive(!menuPanel.activeSelf);
+
+        // Pause or resume the game
+        Time.timeScale = (menuPanel.activeSelf) ? 0f : 1f;
+    }
+}
